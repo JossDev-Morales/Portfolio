bd0c0c5e1be3fe57eaf008f47b7d721f672662f4
Josue Morales <thejosuescript@gmail.com>
2023-02-16 16:14:19 -0600

    responsive and slider fixbugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,10 @@ let copytelefono=document.querySelector(".ptelefono")
 if (width>=1000) {
     img.src="/assets/sources/Logos/photo desktop.png"
 }
-console.log(height);
 
 //services
 let servicesE=document.querySelector(".services")
-let services=[{title:"Design",text:"Diseño de interfaces con perspectiva UX/UI",num:1,svg:`<svg width="110" height="111" viewBox="0 0 110 111" fill="none" xmlns="http://www.w3.org/2000/svg">
+let services=[{title:"Design",text:"Diseño de interfaces con perspectiva UX/UI",num:0,svg:`<svg width="110" height="111" viewBox="0 0 110 111" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M55.0001 9.22222C29.6863 9.22222 9.16675 29.8662 9.16675 55.3333C9.16675 80.8005 29.6863 101.444 55.0001 101.444C80.3138 101.444 100.833 80.8005 100.833 55.3333C100.833 29.8662 80.3138 9.22222 55.0001 9.22222Z" fill="white" stroke="#34B0BE" stroke-width="7" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M36.6667 97.608V64.5556L55.0001 32.2778L73.3334 64.5556V97.608" fill="#34B0BE"/>
 <path d="M36.6667 97.608V64.5556L55.0001 32.2778L73.3334 64.5556V97.608" stroke="#34B0BE" stroke-width="7" stroke-linecap="round" stroke-linejoin="round"/>
@@ -19,8 +18,8 @@ let services=[{title:"Design",text:"Diseño de interfaces con perspectiva UX/UI"
 <path d="M36.6667 64.5555C36.6667 64.5555 41.8322 69.1667 45.8334 69.1667C49.8347 69.1667 55.0001 64.5555 55.0001 64.5555C55.0001 64.5555 60.1655 69.1667 64.1667 69.1667C68.168 69.1667 73.3334 64.5555 73.3334 64.5555" stroke="white" stroke-width="7" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 `},
-{title:"Developing",text:"Desarrollo de proyectos e implementacion de ideas adaptadas a la web",num:2},
-{title:"research",text:"Investigacion de mercado, diseño y metricas",num:3,svg:`<svg width="110" height="110" viewBox="0 0 110 110" fill="none" xmlns="http://www.w3.org/2000/svg">
+{title:"Developing",text:"Desarrollo de proyectos e implementacion de ideas adaptadas a la web",num:1},
+{title:"research",text:"Investigacion de mercado, diseño y metricas",num:2,svg:`<svg width="110" height="110" viewBox="0 0 110 110" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M50.4167 86.5878C70.6671 86.5878 87.0833 70.265 87.0833 50.1298C87.0833 29.9946 70.6671 13.6718 50.4167 13.6718C30.1662 13.6718 13.75 29.9946 13.75 50.1298C13.75 70.265 30.1662 86.5878 50.4167 86.5878Z" stroke="#34B0BE" stroke-width="9" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M96.25 95.7023L76.3125 75.8783" stroke="#34B0BE" stroke-width="9" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
@@ -28,7 +27,7 @@ let services=[{title:"Design",text:"Diseño de interfaces con perspectiva UX/UI"
 ]
 function createServices(service) {
     const container=document.createElement("div")
-    container.classList.add("cont-services","display-IF",`service${service.num}`,"cont-serv-position")
+    container.classList.add("cont-services","display-IF",`servicejs${service.num}`,"cont-serv-position")
     
     const i=document.createElement("i")
     i.classList.add("fa-solid","fa-code")
@@ -36,7 +35,7 @@ function createServices(service) {
     title.textContent=service.title
     const text=document.createElement("p")
     text.textContent=service.text
-    if (container.classList.contains("service2")) {
+    if (container.classList.contains("servicejs1")) {
         container.insertAdjacentElement("beforeend",i)
     }else{
         container.insertAdjacentHTML("beforeend",service.svg)
@@ -51,10 +50,7 @@ function createServices(service) {
 if (width<=1000) {
     document.addEventListener("DOMContentLoaded",()=>{
         let index=0
-        if (index==0) {
-            createServices(services[0])
-            index++
-        }
+        createServices(services[index])
         const leftArrow=document.createElement("div")
         leftArrow.classList.add("arrow","left")
         leftArrow.insertAdjacentHTML("beforeend",'<i class="fa-solid fa-chevron-left"></i>')
@@ -63,19 +59,18 @@ if (width<=1000) {
         rightArrow.insertAdjacentHTML("beforeend",'<i class="fa-solid fa-chevron-right"></i>')
     
         leftArrow.addEventListener("click",()=>{
+            document.querySelector(`.servicejs${index}`).remove()
+            if (index==0) {
+            index=2
+            }else{index--}
             createServices(services[index])
-            index--
-            console.log(index);
-            if (index==-1) {
-                index=2
-            }
         })
         rightArrow.addEventListener("click",()=>{
+            document.querySelector(`.servicejs${index}`).remove()
+            if (index==2) {
+                index=0
+            }else{index++}
             createServices(services[index])
-            index++
-            if (index==3) {
-                index= 0
-            }
         })
         servicesE.insertAdjacentElement("beforeend",leftArrow)
         servicesE.insertAdjacentElement("beforeend",rightArrow)
--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,6 +4,7 @@ let svgmenu=document.querySelector(".menubtn")
 let lineBar1=document.querySelector(".bar-1")
 let lineBar2=document.querySelector(".bar-2")
 let lineBar3=document.querySelector(".bar-3")
+let menuitem=document.querySelectorAll(".nav__listitem")
 document.querySelector(".menubtn").addEventListener("click",()=>{
     setTimeout(() => {
         if (svgmenu.classList.contains("event-menubtn1")) {
@@ -30,4 +31,33 @@ document.querySelector(".menubtn").addEventListener("click",()=>{
             ul.classList.remove("event-menu2")
         }else{ul.classList.add("event-menu2")}
     },200)
+})
+menuitem.forEach(e=>{
+    e.addEventListener("click",()=>{
+        setTimeout(() => {
+            if (svgmenu.classList.contains("event-menubtn1")) {
+                
+                setTimeout(() => {
+                    svgmenu.classList.remove("event-menubtn1")
+                    svgmenu.classList.add("event-menubtn1-post")
+                }, 400);
+            }else{svgmenu.classList.remove("event-menubtn1-post");svgmenu.classList.add("event-menubtn1")}
+            setTimeout(() => {
+                if (svgmenu.classList.contains("event-menubtn2")) {
+                    svgmenu.classList.remove("event-menubtn2")
+                    svgmenu.classList.add("event-menubtn2-post")
+                }else{svgmenu.classList.remove("event-menubtn2-post");svgmenu.classList.add("event-menubtn2")}
+            }, 100);
+        }, 200);
+        ul.classList.remove("event-menu3")
+        if (ul.classList.contains("event-menu1")) {
+            setTimeout(()=>{ul.classList.remove("event-menu1")},1000)
+        }else{ul.classList.add("event-menu1")}
+        setTimeout(()=>{
+            if (ul.classList.contains("event-menu2")) {
+                ul.classList.add("event-menu3")
+                ul.classList.remove("event-menu2")
+            }else{ul.classList.add("event-menu2")}
+        },200)
+    })
 })
